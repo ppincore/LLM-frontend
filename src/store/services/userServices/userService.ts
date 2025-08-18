f78cf0580ed6655c1aad561e1b6d56c35204b4ce
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-18 17:34:15 +0300

    feat: create user upi query

---
+++ src/store/services/userServices/userService.ts
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { TLoginData } from './types';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const userApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+  }),
+  tagTypes: ['User'],
+  endpoints: (build) => ({
+    loginUser: build.mutation({
+      query: (loginData: TLoginData) => ({
+        url: 'login',
+        method: 'POST',
+        body: loginData,
+      }),
+    }),
+  }),
+});
