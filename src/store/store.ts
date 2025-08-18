b7ace9b45848947fb6162ee4a9d83cec67452a43
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-18 17:34:36 +0300

    feat: create store

---
+++ src/store/store.ts
@@ -0,0 +1,17 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { userApi } from './services/userServices/userService';
+
+const rootReducer = combineReducers({
+  [userApi.reducerPath]: userApi.reducer
+});
+
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware)
+  });
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
