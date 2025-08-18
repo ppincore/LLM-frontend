2d54ba4d7a2d604bba18e9e6b3356c6542c17dd3
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-18 17:16:34 +0300

    feat: add store provider in app

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
 import './components/App/styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
+
+const store = setupStore();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
