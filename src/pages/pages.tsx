c5f65c9d4357f8e9b5c4b919e151d1ec2a9bd2fe
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-22 15:39:42 +0300

    feat: add main page

--- src/pages/pages.tsx
+++ src/pages/pages.tsx
@@ -1,2 +1,3 @@
 export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
 export { default as LoginPage } from './LoginPage/LoginPage';
+export {default as MainPage} from './MainPage/MainPage'
