108447383f9b6ca2d27972d6483d24123b605eff
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-26 16:49:41 +0300

    feat: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from 'path'
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -7,4 +8,15 @@ export default defineConfig({
   server: {
     port: 5000,
   },
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app/'),
+      '@entities': path.resolve(__dirname, './src/entities/'),
+      '@features': path.resolve(__dirname, './src/features/'),
+      '@pages': path.resolve(__dirname, './src/pages/'),
+      '@shared': path.resolve(__dirname, './src/shared/'),
+      '@store': path.resolve(__dirname, './src/store/'),
+      '@widgets': path.resolve(__dirname, './src/widgets/'),
+    }
+  }
 });
