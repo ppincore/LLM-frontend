f9845f43fe5af98b01d6cc2f64e53a8ff2008ad1
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-26 16:50:01 +0300

    feat: add user init dispatch

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,10 @@ import './app/styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { setupStore } from './store/store';
+import { userActions } from './entities/User/model/slice/userSlice';
 
 const store = setupStore();
+store.dispatch(userActions.initAuthData())
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
