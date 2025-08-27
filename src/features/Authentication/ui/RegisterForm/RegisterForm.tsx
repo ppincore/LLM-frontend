75944389d592fb94b8c6b952da187820b5f0c0b1
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-27 16:40:56 +0300

    feat: create register form markup

--- src/features/Authentication/ui/RegisterForm/RegisterForm.tsx
+++ src/features/Authentication/ui/RegisterForm/RegisterForm.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { rules } from "@shared/lib/utils/rules";
+import { Form, Input, Button, Typography } from "antd";
+
+const { Text } = Typography;
 
 const RegisterForm = () => {
   return (
-    <div>
-      
-    </div>
+    <Form>
+      <Form.Item name="email" rules={[rules.required("Email is required")]}>
+        <Input placeholder="email" />
+      </Form.Item>
+      <Form.Item name="password" rules={[rules.required("Password is required")]}>
+        <Input placeholder="password" />
+      </Form.Item>
+      <Form.Item rules={[rules.required("Repeat password")]}>
+        <Input placeholder="repeat password" />
+      </Form.Item>
+      <Form.Item>
+        <Button type='primary' children={'Sign up'}/>
+      </Form.Item>
+    </Form>
   );
 };
 
