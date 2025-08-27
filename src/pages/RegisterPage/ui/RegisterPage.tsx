a4e92bd3c318179b17e4a019a96fd6b201f352e1
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-27 16:39:35 +0300

    feat: add register form in register page

--- src/pages/RegisterPage/ui/RegisterPage.tsx
+++ src/pages/RegisterPage/ui/RegisterPage.tsx
@@ -1,10 +1,12 @@
-import { Layout,Row, Card } from "antd";
+import { Layout, Row, Card } from "antd";
+import { RegisterForm } from "@features/Authentication";
 
 const RegisterPage = () => {
   return (
     <Layout>
-      <Row justify='center' align='middle' className="h100">
+      <Row justify="center" align="middle" className="h100">
         <Card>
+          <RegisterForm/>
         </Card>
       </Row>
     </Layout>
