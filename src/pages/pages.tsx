bb3faab79454715b648449651f8da00e519039f8
Bogdan gaevskij <liltracyn@yandex.ru>
2025-08-27 14:44:59 +0300

    feat: create Register page component

--- src/pages/pages.tsx
+++ src/pages/pages.tsx
@@ -1,3 +1,4 @@
 export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
 export { default as LoginPage } from './LoginPage/LoginPage';
 export {default as MainPage} from './MainPage/MainPage'
+export {default as RegisterPage} from './RegisterPage/RegisterPage'
